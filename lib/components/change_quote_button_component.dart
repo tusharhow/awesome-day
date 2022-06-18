3142a59e966572334b0c974f346217e565387316
tusharhow <tusharsoftdev@gmail.com>
2022-06-18 08:34:53 -0700

    Issue fix

--- lib/components/change_quote_button_component.dart
+++ lib/components/change_quote_button_component.dart
@@ -9,49 +9,45 @@ class ChangeQuoteButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<QuoteController>(
-   
-      builder: (controller) {
-        return GestureDetector(
-          onTap: () {
-            controller.randomQuote();
-          },
-          child: Container(
-            height: 65,
-            width: MediaQuery.of(context).size.width / 2.1,
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              border: Border.all(
-                color: Colors.black12,
-                width: 1,
-              ),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              ),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  offset: Offset(0, 10),
-                ),
-              ],
+    return GetBuilder<QuoteController>(builder: (controller) {
+      return GestureDetector(
+        onTap: () {
+          controller.randomQuote();
+        },
+        child: Container(
+          height: 65,
+          width: MediaQuery.of(context).size.width / 2.1,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            border: Border.all(
+              color: Colors.black12,
+              width: 1,
             ),
-            child: const Center(
-              child: Text(
-                'Change Quote',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1,
-                ),
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10,
+                offset: Offset(0, 10),
+              ),
+            ],
+          ),
+          child: const Center(
+            child: Text(
+              'Change Quote',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
               ),
             ),
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'views/homepage.dart';
 
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Awesome Day',
       theme: ThemeData(
